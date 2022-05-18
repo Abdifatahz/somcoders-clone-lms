72eddc7c207d3cef8706308e665c3ec2eae23f36
Abdifatahz <faahid44@gmail.com>
2022-05-18 12:17:43 +0300

    Added basic routes.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return "Home";
+});
+
+Route::get("/courses", function () {
+    return "Courses";
+});
+
+Route::get("categories", function () {
+    return "Categories";
+});
+
+Route::get("about", function () {
+    return "about";
 });
