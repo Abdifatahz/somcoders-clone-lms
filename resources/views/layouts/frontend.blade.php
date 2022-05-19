d61c6c86162f11d4cc22fb9b0446a2c22a6710f4
Abdifatahz <faahid44@gmail.com>
2022-05-19 13:31:16 +0300

    Fix broken css styles on show pages.

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -12,7 +12,7 @@
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
 </head>
 
 <body class="d-flex flex-column h-100">
@@ -20,14 +20,14 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-navy">
             <div class="container px-5">
-                <a class="navbar-brand" href="index.html">Somclone</a>
+                <a class="navbar-brand" href="{{ url('/') }}">Somclone</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation"><span
                         class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
 
                         <li class="nav-item"><a class="nav-link" href="faq.html">Categories</a></li>
                         <li class="nav-item"><a class="nav-link" href="faq.html">Courses</a></li>
@@ -43,29 +43,6 @@
                 </div>
             </div>
         </nav>
-        <!-- Header-->
-        <header class="bg-navy py-5">
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center justify-content-center">
-                    <div class="col-lg-8 col-xl-7 col-xxl-6">
-                        <div class="my-5 text-center text-xl-start">
-                            <h1 class="display-5 fw-bolder text-white mb-2">Koorsooyin tayo leh oo ay diyaariyeen
-                                bareyaal aqoon uleh
-                            </h1>
-                            <p class="lead fw-normal text-white-50 mb-4">KUBARO AF-KAAGA HOOYO
-                                PROGRAMMING
-                                DATABASES
-                                GRAPHIC DESIGN</p>
-                            <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                                <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5"
-                            src="{{ asset('images/profile-linkedin.jpg') }}" alt="..." /></div>
-                </div>
-            </div>
-        </header>
 
         @yield('content')
 
@@ -90,7 +67,7 @@
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
-    <script src="js/scripts.js"></script>
+    <script src="{{ asset('js/scripts.js') }}"></script>
 </body>
 
 </html>
